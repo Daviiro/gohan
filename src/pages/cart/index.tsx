f1890a996ebbbeb09c28332977d6ef24f6ce3771
David Jr <david.junior@unesp.br>
2023-03-21 00:20:59 -0300

    feat: styling the cart page

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,17 @@
 import DefaultLayout from "@/components/layout";
+import ItemList from "@/components/itemList";
 
 const Cart: React.FC = () => {
-	return <DefaultLayout>Cart page</DefaultLayout>;
+	return (
+		<DefaultLayout>
+			<div className="grid grid-cols-[5fr_3fr] h-[93.5vh] overflow-hidden">
+				<ItemList />
+				<div className="bg-gray-100 text-black overflow-y-hidden h-[94vh]">
+					<span>parte extra</span>
+				</div>
+			</div>
+		</DefaultLayout>
+	);
 };
 
 export default Cart;
