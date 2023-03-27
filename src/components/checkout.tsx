dc8bd5cd99ef300f136ff55b7acc82608d73589f
David Jr <david.junior@unesp.br>
2023-03-27 11:56:26 -0300

    feat: value per person

--- src/components/checkout.tsx
+++ src/components/checkout.tsx
@@ -1,7 +1,9 @@
+import { RootState } from "@/store/store";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { BiGroup, BiMoneyWithdraw } from "react-icons/bi";
 import { BsChatText } from "react-icons/bs";
+import { useSelector } from "react-redux";
 
 const Checkout: React.FC = () => {
 	const { t } = useTranslation();
@@ -17,6 +19,16 @@ const Checkout: React.FC = () => {
 		}
 	};
 
+	const itemsOnCart = useSelector((state: RootState) => state.cart);
+	const orderTotal = itemsOnCart.reduce((accumulator, object) => {
+		return accumulator + object.price * object.amount;
+	}, 0);
+
+	const valuePerPerson =
+		peopleAmount > 1
+			? Math.round((orderTotal / peopleAmount) * 100) / 100
+			: orderTotal;
+
 	return (
 		<div className="h-full flex flex-col justify-between">
 			<div className="p-5 h-1/2 w-full text-center flex flex-col justify-start min-h-[12rem]">
@@ -26,40 +38,47 @@ const Checkout: React.FC = () => {
 						{t("costPerPerson")}
 					</span>
 				</div>
-				<div className="w-1/2">
-					<label
-						htmlFor="custom-input-number"
-						className="w-full text-gray-700 text-sm font-semibold"
-					>
-						{t("numberOfPeople")}
-					</label>
-					<div className="flex flex-row h-14 w-full rounded-lg relative bg-transparent mt-1 border-2 border-red-700">
-						<button
-							data-action="decrement"
-							className="text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
-							onClick={() => handlePeopleDecrement()}
+				<div className="w-full flex">
+					<div className="w-1/2">
+						<label
+							htmlFor="custom-input-number"
+							className="w-full text-gray-700 text-sm font-semibold"
 						>
-							<span className="m-auto text-2xl font-semibold text-red-700">
-								−
-							</span>
-						</button>
-						<input
-							type="number"
-							className="outline-none focus:outline-none text-center w-full bg-transparent font-semibold text-2xl hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-black  outline-none hide-spin"
-							name="custom-input-number"
-							value={peopleAmount}
-							readOnly
-							min={1}
-						/>
-						<button
-							data-action="increment"
-							className="text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
-							onClick={() => handlePeopleIncrement()}
-						>
-							<span className="m-auto text-2xl font-semibold text-red-700">
-								+
-							</span>
-						</button>
+							{t("numberOfPeople")}
+						</label>
+						<div className="flex flex-row h-14 w-full rounded-lg relative bg-transparent mt-1 border-2 border-red-700">
+							<button
+								data-action="decrement"
+								className="text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
+								onClick={() => handlePeopleDecrement()}
+							>
+								<span className="m-auto text-2xl font-semibold text-red-700">
+									−
+								</span>
+							</button>
+							<input
+								type="number"
+								className="outline-none focus:outline-none text-center w-full bg-transparent font-semibold text-2xl hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-black  outline-none hide-spin"
+								name="custom-input-number"
+								value={peopleAmount}
+								readOnly
+								min={1}
+							/>
+							<button
+								data-action="increment"
+								className="text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
+								onClick={() => handlePeopleIncrement()}
+							>
+								<span className="m-auto text-2xl font-semibold text-red-700">
+									+
+								</span>
+							</button>
+						</div>
+					</div>
+					<div className="w-1/2 flex justify-center items-center">
+						<span className="font-bold text-4xl font-bebas">
+							R$ {valuePerPerson.toFixed(2)}
+						</span>
 					</div>
 				</div>
 			</div>
